d21cc44f94ae89c04ab2b6dc94ce263347bf5a49
nemetho <nemethotto.123@gmail.com>
2017-12-31 13:26:32 +0100

    fix: _make_git_tar.sh regular expression '.' match + new make-git-targz script which creates 'subdirectory' in archive

--- scripts/_make_git_tar.sh
+++ scripts/_make_git_tar.sh
@@ -1,3 +1,5 @@
 [ -z "$1" ] && exit 1
-LIST=`ls -a | grep -v "^.$\|^..$\|^.git$\|^_make_git_tar.sh$"`
+LIST=`ls -A | grep -v "^\.git$\|^_make_git_tar\.sh$"`
 tar czf $1.tar.gz $LIST
+
+#ls -A | grep -v "^\.git$\|^_make_git_tar\.sh$" | xargs -r tar czf $1.tar.gz
---
+++ scripts/_make_git_tar_dir.sh
@@ -0,0 +1,5 @@
+[ -z "$1" ] && exit 1
+#LIST=`ls -A | grep -v "^\.git$\|^_make_git_tar_dir\.sh$"`
+#tar czf $1.tar.gz --transform="s%^%$1/%" $LIST
+
+ls -A | grep -v "^\.git$\|^_make_git_tar_dir\.sh$" | xargs -r tar czf $1.tar.gz --transform="s%^%$1/%"
