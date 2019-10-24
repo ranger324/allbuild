a06daa30bd9784d45329c811b3cec80fb71becb7
Németh Ottó <nemethotto.123@gmail.com>
2019-10-24 19:57:31 +0200

    various changes

--- scripts/_screen.sh
+++ scripts/_screen.sh
@@ -1,2 +1,2 @@
-[ -z "$1" ] && echo "Add user" && exit 1
+[ -z "$1" ] && echo "Add username as parameter" && exit 1
 openvt -c 8 -s -- sudo -u $1 -- screen sh -c 'cd; exec bash -l'
--- scripts/_thermal.sh
+++ scripts/_thermal.sh
@@ -1,9 +1,9 @@
-thermal="/sys/class/hwmon/hwmon0/temp1_input"
+thermal=`ls /sys/class/hwmon/*/temp1_input 2> /dev/null | head -n 1`
 if [ -e $thermal ]; then
     therm=$(cat $thermal)
     temp=$(echo "scale=1; $therm/1000" | bc)
 fi
 if [ -n "$temp" ]; then
     cpu="[${temp}°C]"
+    echo "$cpu"
 fi
-echo "$cpu"
