d72cd82c9c81c5fe4572e99c9debba1fa1388648
Németh Ottó <nemethotto.123@gmail.com>
2019-11-17 17:01:39 +0100

    some modification on instpkg

--- buildroot/_pkg/dest/pkg_check/_ver_check.sh
+++ buildroot/_pkg/dest/pkg_check/_ver_check.sh
@@ -5,7 +5,7 @@ cd /dest
 ls *.tar.gz > /dev/null 2>&1 || exit 1
 ls *.tar.gz | sed 's/\.tar\.gz$//' | rev | sed 's/\([^-]\+\)-\([^-]\+\)-\(.*\)$/\1 \2 \3/' | rev | sort -V -r -k 2,3 | \
 while read name ver rev; do
-    if ! echo "$processed" | grep -qx "$name"; then
+    if ! echo "$processed" | grep -q "^$name$"; then
 	echo "$name $ver $rev"
 	mv /dest/"$name-$ver-$rev.tar.gz" /dest/new
 	[ -z "$processed" ] && processed="$name" || processed=`echo -ne "$processed\n$name"`
